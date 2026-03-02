368f12579bad1f0a2fb259fc9c44beecfedba63f
night-slayer18 <samanuaia257@gmail.com>
2026-03-02 23:14:05 +0530

    test(coverage): cover hex/url decode error branches
    Signed-off-by: night-slayer18 <samanuaia257@gmail.com>

--- tests/encodings_test.go
+++ tests/encodings_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestBase64_RoundTrip(t *testing.T) {
 	input := "Hello, World!"
-	enc, err := processors.Base64Encode{}.Transform([]byte(input))
+	enc, err := (processors.Base64Encode{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.Base64Decode{}.Transform([]byte(enc))
+	dec, err := (processors.Base64Decode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -23,11 +23,11 @@ func TestBase64_RoundTrip(t *testing.T) {
 
 func TestBase32_RoundTrip(t *testing.T) {
 	input := "hello"
-	enc, err := processors.Base32Encoding{}.Transform([]byte(input))
+	enc, err := (processors.Base32Encoding{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.Base32Decode{}.Transform([]byte(enc))
+	dec, err := (processors.Base32Decode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("base32 decode error: %v", err)
 	}
@@ -38,11 +38,11 @@ func TestBase32_RoundTrip(t *testing.T) {
 
 func TestBase64URL_RoundTrip(t *testing.T) {
 	input := "Hello, World!"
-	enc, err := processors.Base64URLEncode{}.Transform([]byte(input))
+	enc, err := (processors.Base64URLEncode{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.Base64URLDecode{}.Transform([]byte(enc))
+	dec, err := (processors.Base64URLDecode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("base64url decode error: %v", err)
 	}
@@ -53,11 +53,11 @@ func TestBase64URL_RoundTrip(t *testing.T) {
 
 func TestHexEncode_RoundTrip(t *testing.T) {
 	input := "hello"
-	enc, err := processors.HexEncode{}.Transform([]byte(input))
+	enc, err := (processors.HexEncode{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.HexDecode{}.Transform([]byte(enc))
+	dec, err := (processors.HexDecode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("hex decode error: %v", err)
 	}
@@ -68,11 +68,11 @@ func TestHexEncode_RoundTrip(t *testing.T) {
 
 func TestBinary_RoundTrip(t *testing.T) {
 	input := "hi"
-	enc, err := processors.BinaryEncode{}.Transform([]byte(input))
+	enc, err := (processors.BinaryEncode{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.BinaryDecode{}.Transform([]byte(enc))
+	dec, err := (processors.BinaryDecode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("binary decode error: %v", err)
 	}
@@ -83,11 +83,11 @@ func TestBinary_RoundTrip(t *testing.T) {
 
 func TestBase58_RoundTrip(t *testing.T) {
 	input := "hello"
-	enc, err := processors.Base58Encode{}.Transform([]byte(input))
+	enc, err := (processors.Base58Encode{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.Base58Decode{}.Transform([]byte(enc))
+	dec, err := (processors.Base58Decode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("base58 decode error: %v", err)
 	}
@@ -98,11 +98,11 @@ func TestBase58_RoundTrip(t *testing.T) {
 
 func TestBase62_RoundTrip(t *testing.T) {
 	input := "hello"
-	enc, err := processors.Base62Encode{}.Transform([]byte(input))
+	enc, err := (processors.Base62Encode{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.Base62Decode{}.Transform([]byte(enc))
+	dec, err := (processors.Base62Decode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("base62 decode error: %v", err)
 	}
@@ -113,11 +113,11 @@ func TestBase62_RoundTrip(t *testing.T) {
 
 func TestASCII85_RoundTrip(t *testing.T) {
 	input := "hello world"
-	enc, err := processors.ASCII85Encoding{}.Transform([]byte(input))
+	enc, err := (processors.ASCII85Encoding{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.ASCII85Decoding{}.Transform([]byte(enc))
+	dec, err := (processors.ASCII85Decoding{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("ascii85 decode error: %v", err)
 	}
@@ -128,11 +128,11 @@ func TestASCII85_RoundTrip(t *testing.T) {
 
 func TestCrockfordBase32_RoundTrip(t *testing.T) {
 	input := "hello"
-	enc, err := processors.CrockfordBase32Encode{}.Transform([]byte(input))
+	enc, err := (processors.CrockfordBase32Encode{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.CrockfordBase32Decode{}.Transform([]byte(enc))
+	dec, err := (processors.CrockfordBase32Decode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("crockford decode error: %v", err)
 	}
@@ -143,11 +143,11 @@ func TestCrockfordBase32_RoundTrip(t *testing.T) {
 
 func TestURL_RoundTrip(t *testing.T) {
 	input := "hello world & more"
-	enc, err := processors.URLEncode{}.Transform([]byte(input))
+	enc, err := (processors.URLEncode{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.URLDecode{}.Transform([]byte(enc))
+	dec, err := (processors.URLDecode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("url decode error: %v", err)
 	}
@@ -158,15 +158,31 @@ func TestURL_RoundTrip(t *testing.T) {
 
 func TestHTML_RoundTrip(t *testing.T) {
 	input := `<hello> & "world"`
-	enc, err := processors.HTMLEncode{}.Transform([]byte(input))
+	enc, err := (processors.HTMLEncode{}).Transform([]byte(input))
 	if err != nil {
 		t.Fatal(err)
 	}
-	dec, err := processors.HTMLDecode{}.Transform([]byte(enc))
+	dec, err := (processors.HTMLDecode{}).Transform([]byte(enc))
 	if err != nil {
 		t.Fatalf("html decode error: %v", err)
 	}
 	if dec != input {
 		t.Errorf("html roundtrip: got %q, want %q", dec, input)
+	}
+}
+
+func TestHexDecode_InvalidInput(t *testing.T) {
+	if _, err := (processors.HexDecode{}).Transform([]byte("zz")); err == nil {
+		t.Fatal("expected hex decode error for invalid input")
+	}
+}
+
+func TestURLDecode_MalformedPercent(t *testing.T) {
+	got, err := (processors.URLDecode{}).Transform([]byte("%zz"))
+	if err != nil {
+		t.Fatalf("url-decode should not return errors, got: %v", err)
+	}
+	if got != "" {
+		t.Fatalf("expected empty output for malformed query escape, got %q", got)
 	}
 }
